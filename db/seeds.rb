77e59f0965502ada6157f68ec28ed10c4d8c0257
Anuj <anuj.dhiman@antechindia.com>
2019-10-31 18:49:43 +0530

    removed extra code rake task

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,3 @@ Rake::Task['seed:blockchains'].invoke
 Rake::Task['seed:currencies'].invoke
 Rake::Task['seed:markets'].invoke
 # Rake::Task['seed:wallets'].invoke
-Rake::Task['seed:trading_fees'].invoke
