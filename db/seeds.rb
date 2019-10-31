86edf1f8a9615535dd32105eb82ba338d9fb765e
Anuj <anuj.dhiman@antechindia.com>
2019-10-31 17:10:33 +0530

    changes for seeds.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
 # NOTE: The order of task matters because Currency belongs_to Blockchain.
 Rake::Task['seed:accounts'].invoke
 Rake::Task['seed:blockchains'].invoke
-Rake::Task['seed:currencies'].invoke
+# Rake::Task['seed:currencies'].invoke
 Rake::Task['seed:markets'].invoke
 Rake::Task['seed:wallets'].invoke
 Rake::Task['seed:trading_fees'].invoke
